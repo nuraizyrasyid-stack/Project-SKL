300d5223328a6f0cb05d280b4969b0c161e29153
rasyid nur aisy <nuraizyrasyid@gmail.com>
2026-06-12 20:12:01 +0700

    Prepare project for deployment

--- perpus_project/settings.py
+++ perpus_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+3@5c=d1p%u2o)0qhp0ovmu!20=klbghd)of*$fq-20uftg0=c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
